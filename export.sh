e8dd338f625c82d17a08037343813d96559c0da7
ychnh <yechanhong@gmail.com>
2023-06-14 16:28:37 +0900

    update

---
+++ export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for org_file in *.org; do
+  html_file="${org_file%.org}.html"
+  emacs -batch "$org_file" -f org-html-export-to-html --kill
+  mv "${org_file%.org}.html" "$html_file"
+  echo "Exported $org_file to $html_file"
+done
+
