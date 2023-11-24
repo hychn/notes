c893c24898c135affc753a95ead9bee3fde8a906
hychn <daemon.ychnh@gmail.com>
2023-11-23 21:43:59 -0600

    updating

--- export.sh
+++ export.sh
@@ -3,7 +3,7 @@
 for org_file in *.org; do
   html_file="${org_file%.org}.html"
   emacs -batch "$org_file" -f org-html-export-to-html --kill
-  mv "${org_file%.org}.html" "$html_file"
+  mv "${org_file%.org}.html" "html/${html_file}"
   echo "Exported $org_file to $html_file"
 done
 
