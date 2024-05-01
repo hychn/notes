0764cdf799201a6c035dcd7b971bcf4fd3ea6a82
ychnh <yechanhong@gmail.com>
2024-04-30 21:30:59 -0500

    updating

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for org_file in *.org; do
+for org_file in algebra.org; do
   html_file="${org_file%.org}.html"
   emacs -batch "$org_file" -f org-html-export-to-html --kill
   mv "${org_file%.org}.html" "html/${html_file}"
